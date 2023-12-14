f9baf6ca5ff31ec299461966718a7f3a7c676ee6
Leeiss <farrakhova.2004@mail.ru>
2023-12-15 00:43:15 +0300

    add

---
+++ rectangle.h
@@ -0,0 +1,44 @@
+#include <stdexcept>
+
+class Rectangle
+{
+    int Length;
+    int Breadth;
+    public:
+        Rectangle(int l, int b)
+        {
+            Length = l;
+            Breadth = b;
+            if (Length <= 0 || Breadth <= 0) {
+            throw std::invalid_argument("Invalid parametrs for rectangle");
+        }
+        }
+        int Area()
+        {
+            return Length * Breadth;
+        }
+        // отношение длинны к ширине
+        double RelationsOfTheParties()
+        {
+            if (Breadth == 0) {
+                throw std::runtime_error("zero division");
+            }
+            return Length / Breadth;
+        }
+};
+
+int CalculateRectArea(int l, int b)
+{
+    Rectangle *p = new Rectangle(l, b);
+    int area = p->Area();
+    delete p;
+    return area;
+}
+
+double CalculateRectRelationsOfTheParties(int l, int b)
+{
+    Rectangle *p = new Rectangle(l, b); 
+    double relation = p->RelationsOfTheParties();
+    delete p;
+    return relation;
+}
